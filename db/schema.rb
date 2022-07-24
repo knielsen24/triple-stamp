2b94700b227e349c359053b281323d100ecf0683
Kevin Nielsen <knielsen24@gmail.com>
2022-07-24 11:19:29 -0700

    faker

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_24_010035) do
+ActiveRecord::Schema.define(version: 2022_07_24_171123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "postal_code"
+    t.string "country"
+    t.string "image"
+    t.integer "units"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
@@ -27,4 +42,5 @@ ActiveRecord::Schema.define(version: 2022_07_24_010035) do
     t.string "business"
   end
 
+  add_foreign_key "properties", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,18 @@
 puts "🌱 double stamping..."
+# :name, :address, :city, :state, :postal_code, :country, :image, :units
+
+200.times do
+    Property.create(
+        name: Faker::Address.community,
+        address: Faker::Address.street_address,
+        city: Faker::Address.city,
+        postal_code: Faker::Address.postcode,
+        country: Faker::Address.country,
+        units: rand(1..50),
+        image: Faker::LoremFlickr.image(search_terms: ['business']),
+        user_id: rand(1..10),
+    )
+end
 
 puts "🌱 triple stamping..."
 puts "✅✅✅ You can't triple stamp a double stamp! Loyld!"
