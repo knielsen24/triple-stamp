a9e40c99168552751d8a0033ef2144877e5c3355
Kevin Nielsen <knielsen24@gmail.com>
2022-07-30 15:13:28 -0700

    table!

--- client/src/Components/InspectionsPage/PropertyAllInspectionsList.js
+++ client/src/Components/InspectionsPage/PropertyAllInspectionsList.js
@@ -1,40 +1,9 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { setSelectProperty } from "../../app/features/propertySlice";
 import { useFetchPropInspectionsQuery } from "../../app/services/propertyApiSlice";
+import PropertyAllInspecitonsTable from "../Tables/PropertyAllInspecitonsTable";
 
 function PropertyAllInspectionsList() {
-    const property = useSelector(setSelectProperty);
-    const { data: propInspections } = useFetchPropInspectionsQuery(
-        property.id || ""
-    );
-
-    let allPropInspect;
-
-    if (propInspections) {
-        console.log(propInspections);
-        allPropInspect = propInspections.map((inspect) => {
-            return (
-                <ul
-                    className="list-group list-group-horizontal"
-                    key={inspect.id}
-                >
-                    <li className="list-group-item flex-fill rounded-0">
-                        {inspect.title}
-                    </li>
-                    <li className="list-group-item flex-fill rounded-0">
-                        {inspect.type_name}
-                    </li>
-                    <li className="list-group-item flex-fill rounded-0">
-                        {inspect.status}
-                    </li>
-                    <li className="list-group-item flex-fill rounded-0">
-                        {inspect.scheduled_date}
-                    </li>
-                </ul>
-            );
-        });
-    }
 
     return (
         <div className="accordion-item">
@@ -55,22 +24,9 @@ function PropertyAllInspectionsList() {
                 className="accordion-collapse collapse"
                 aria-labelledby="panelsStayOpen-headingTwo"
             >
-                <div className="accordion-body bg-light bg-opacity-75">
-                    <ul className="list-group list-group-horizontal flex-fill text-center">
-                        <li className="list-group-item bg-secondary rounded-0 bg-opacity-50 flex-fill ">
-                            Title
-                        </li>
-                        <li className="list-group-item bg-secondary rounded-0 bg-opacity-50 flex-fill">
-                            Type
-                        </li>
-                        <li className="list-group-item bg-secondary rounded-0 bg-opacity-50 flex-fill">
-                            Status
-                        </li>
-                        <li className="list-group-item bg-secondary rounded-0 bg-opacity-50 flex-fill">
-                            Date
-                        </li>{" "}
-                    </ul>
-                    {allPropInspect}
+                <div className="accordion-body bg-light bg-opacity-50">
+                    <PropertyAllInspecitonsTable />
+
                 </div>
             </div>
         </div>
---
+++ client/src/Components/Tables/PropertyAllInspecitonsTable.js
@@ -0,0 +1,46 @@
+import { useSelector } from "react-redux";
+import { setSelectProperty } from "../../app/features/propertySlice";
+import { useFetchPropInspectionsQuery } from "../../app/services/propertyApiSlice";
+
+function PropertyAllInspecitonsTable() {
+    const property = useSelector(setSelectProperty);
+    const { data: propInspections } = useFetchPropInspectionsQuery(
+        property.id || ""
+    );
+
+    let allPropInspect;
+
+    if (propInspections) {
+        console.log(propInspections);
+        allPropInspect = propInspections.map((inspect) => {
+            return (
+                <tr key={inspect.id}>
+                    <th scope="row">{inspect.unit_id}</th>
+                    <td>{inspect.title}</td>
+                    <td>{inspect.type_name}</td>
+                    <td>{inspect.status}</td>
+                    <td>{inspect.scheduled_date}</td>
+                </tr>
+            );
+        });
+    }
+
+    return (
+        <div>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Unit #</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Scheduled Date</th>
+                    </tr>
+                </thead>
+                <tbody className="table-group-divider">{allPropInspect}</tbody>
+            </table>
+        </div>
+    );
+}
+
+export default PropertyAllInspecitonsTable;
